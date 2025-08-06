e2ef170448a72689fe521f4d07f53c613221d205
sanagui-dj <alexsanagui.00@gmail.com>
2025-08-05 23:15:57 -0600

    Configurar Eleventy para que copie la carpeta de scripts

---
+++ .eleventy.js
@@ -0,0 +1,18 @@
+module.exports = function(eleventyConfig) {
+  
+  // Esta línea le dice a Eleventy:
+  // "Cuando construyas el sitio, busca una carpeta llamada 'scripts'
+  // y cópiala EXACTAMENTE IGUAL dentro de la carpeta _site".
+  eleventyConfig.addPassthroughCopy("scripts");
+
+  // El resto de la configuración que teníamos antes
+  eleventyConfig.addWatchTarget("./_includes/");
+  
+  return {
+    dir: {
+      input: ".",
+      includes: "_includes",
+      output: "_site"
+    }
+  };
+};
