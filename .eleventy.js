86dc8a6ce167b5a95689a6ce1b5f9dc9ac2709b9
sanagui-dj <alexsanagui.00@gmail.com>
2025-08-06 01:50:24 -0600

    agregados los archivitos pa google

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,10 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("scripts");
 eleventyConfig.addPassthroughCopy("css");
 eleventyConfig.addPassthroughCopy("stream.m3u");
+eleventyConfig.addPassthroughCopy("img");
+eleventyConfig.addPassthroughCopy("googlea95dadeac05ead3d.html");
+eleventyConfig.addPassthroughCopy("robots.txt");
+eleventyConfig.addPassthroughCopy("sitemap.xml");
   // El resto de la configuración que teníamos antes
   eleventyConfig.addWatchTarget("./_includes/");
   
