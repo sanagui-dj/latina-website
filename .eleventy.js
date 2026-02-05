74f6d3e3f2e6af624d673c669dfed5dd402a8d31
alejandro <alexsanagui.00@gmail.com>
2026-02-04 23:44:15 -0600

    corrigiendo sitemap

--- .eleventy.js
+++ .eleventy.js
@@ -1,30 +1,19 @@
-const sitemap = require("@quasibit/eleventy-plugin-sitemap");
-
 module.exports = function(eleventyConfig) {
   
-  // 1. Configuración del Plugin de Sitemap
-  eleventyConfig.addPlugin(sitemap, {
-    sitemap: {
-      hostname: "https://latinalive.net",
-    },
-  });
-
-  // 2. Copia de archivos estáticos (Passthrough)
+  // Copia de archivos estáticos (Incluyendo el sitemap manual)
   eleventyConfig.addPassthroughCopy("scripts");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("stream.m3u");
   eleventyConfig.addPassthroughCopy("googlea95dadeac05ead3d.html");
   eleventyConfig.addPassthroughCopy("robots.txt");
+  
+  // ESTA LÍNEA ES CLAVE AHORA:
+  eleventyConfig.addPassthroughCopy("sitemap.xml"); 
 
-  // NOTA: Asegúrate de NO tener un archivo llamado sitemap.xml en la raíz, 
-  // o el plugin no podrá escribir el nuevo.
-
-  // 3. Vigilancia de cambios
   eleventyConfig.addWatchTarget("./_includes/");
   
   return {
-    // Definimos la raíz como pathPrefix para evitar URLs de GitHub o subcarpetas
     pathPrefix: "/", 
     dir: {
       input: ".",
